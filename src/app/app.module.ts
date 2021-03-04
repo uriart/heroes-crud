d7b911098a67ad0e7e7db5be1558af390b4f1690
uriart <loiro_8@hotmail.com>
2021-03-04 21:20:18 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/heroes.service.ts
@@ -0,0 +1,72 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HeroeModel } from '../models/heroe.model';
+import { map, delay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private url = 'https://login-app-886ca-default-rtdb.europe-west1.firebasedatabase.app';
+
+  constructor( private http: HttpClient ) { }
+
+  crearHeroe( heroe: HeroeModel ) {
+  
+    return this.http.post(`${ this.url }/heroes.json`, heroe)
+            .pipe(
+              map( (resp: any) => {
+                heroe.id = resp.name;
+                return heroe;
+              })
+            );
+
+  }
+
+  actualizarHeroe( heroe: HeroeModel ) {
+  
+    const heroeTemp: any = {
+      ...heroe
+    };
+
+    delete heroeTemp.id;
+
+    return this.http.put(`${ this.url }/heroes/${ heroe.id }.json`, heroeTemp);
+            
+  }
+
+  getHeroe(id: string){
+    return this.http.get(`${ this.url }/heroes/${ id }.json`);
+  }
+
+  borrarHeroe( id: string ) {
+    return this.http.delete(`${ this.url }/heroes/${ id }.json`);
+  }
+
+  getHeroes() {
+    return this.http.get(`${ this.url }/heroes.json`)
+      .pipe(
+        map( resp => this.crearArreglo(resp) ),
+        delay(1500)
+      );
+  }
+
+  private crearArreglo( heroesObj: any){
+
+    const heroes: HeroeModel[] = [];
+    console.log(heroesObj);
+
+    Object.keys( heroesObj ).forEach( key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id= key;
+
+      heroes.push ( heroe );
+    });
+
+    if ( heroesObj === null ) { return []; }
+
+    return heroes;
+
+  }
+}
